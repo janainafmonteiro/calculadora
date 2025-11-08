cea5410b1316397532bdf8fde42dd82576ac2fa9
janainafmonteiro <janainaferreiramonteiro@hotmail.com>
2025-11-07 21:44:39 -0300

    Update analisarExpressao.java

--- analisarExpressao.java
+++ analisarExpressao.java
@@ -125,21 +125,21 @@ public class analisarExpressao {
     public String verificarVariaveis(){
         int i = 0;
         while(i < expressao.length()){
-            if(expressao.charAt(i) >= 'a' && expressao.charAt(i) <= 'z'){
+            if(expressao.charAt(i) >= 'a' && expressao.charAt(i) <= 'z' && expressao.charAt(i) != 'i'){
                 if(i==0){
                     if(expressao.length() > 1){
-                        if((expressao.charAt(i+1) >='0' && expressao.charAt(i+1)<='9') || (expressao.charAt(i+1) >= 'a' && expressao.charAt(i+1) <= 'z') || expressao.charAt(i+1) == '('){                       
+                        if((expressao.charAt(i+1) >='0' && expressao.charAt(i+1)<='9') || (expressao.charAt(i+1) >= 'a' && expressao.charAt(i+1) <= 'z' && expressao.charAt(i+1) != 'i') || expressao.charAt(i+1) == '('){                       
                             this.expressao = expressao.substring(0, i+1) + '*' + expressao.substring(i+1);
                             i++;
                         }
                     }
                 }else{
-                    if(expressao.charAt(i-1) >='0' && expressao.charAt(i-1)<='9'|| (expressao.charAt(i-1) >= 'a' && expressao.charAt(i-1) <= 'z') || expressao.charAt(i-1) == ')'){
+                    if(expressao.charAt(i-1) >='0' && expressao.charAt(i-1)<='9'|| (expressao.charAt(i-1) >= 'a' && expressao.charAt(i-1) <= 'z' && expressao.charAt(i-1) != 'i') || expressao.charAt(i-1) == ')'){
                         this.expressao = expressao.substring(0, i) + '*' + expressao.substring(i);
                         i++;
                     } 
                     if(i != expressao.length()-1){
-                        if((expressao.charAt(i+1) >='0' && expressao.charAt(i+1)<='9') || (expressao.charAt(i+1) >= 'a' && expressao.charAt(i+1) <= 'z') || expressao.charAt(i+1) == '('){
+                        if((expressao.charAt(i+1) >='0' && expressao.charAt(i+1)<='9') || (expressao.charAt(i+1) >= 'a' && expressao.charAt(i+1) <= 'z' && expressao.charAt(i+1) != 'i') || expressao.charAt(i+1) == '('){
                             this.expressao = expressao.substring(0, i+1) + '*' + expressao.substring(i+1);
                             i++;
                         }
@@ -153,7 +153,7 @@ public class analisarExpressao {
     public String valorVariaveis(){
         this.expressao = verificarVariaveis();
         for (int i = 0; i < expressao.length(); i++) {
-            if(expressao.charAt(i) >= 'a' && expressao.charAt(i) <= 'z'){              
+            if(expressao.charAt(i) >= 'a' && expressao.charAt(i) <= 'z' && expressao.charAt(i) != 'i'){              
                 System.err.println("Qual sera o valor da variavel: "+ expressao.charAt(i) + " ?");
                 Scanner scan = new Scanner(System.in);
                 double valor = scan.nextDouble();            
